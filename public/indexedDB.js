e641cd20b08719b713252ce9c2dc6804d098894e
Michelle Chou <chou.m.rdh@gmail.com>
2020-07-05 14:48:37 -0700

    saveRecord function

--- public/indexedDB.js
+++ public/indexedDB.js
@@ -6,23 +6,21 @@ dbRequest.onupgradeneeded = event => {
 
     const db = event.target.result;
     const store = db.createObjectStore("budge", { autoIncrement: true });
-    // store.createIndex("statusIndex", "status");
+   
 };
 
 dbRequest.onsuccess = event => {
     const db = event.target.result;
     console.log(`Successfully opened connect to indexedDB :${db.name}`)
-    addData(db)
+
 };
 
 dbRequest.onerror = event => {
     console.log(`Error opening connection to indexedDB: ${event.target.error}`)
 };
-// const addData = (db, data) => {
-//     const budgeStore = db.transaction(["budge"], "readwrite").transcation.objectStore("budge");
-//     budgeStore.add({data});
-// };
 
-// const getAllData = ()=>{
+function saveRecord (record){
+const budgeStore = db.transaction(["budge"], "readwrite").transcation.objectStore("budge");
+    budgeStore.add(record);
+}
 
-// };
