2251fdc1eddbe6d5e912ea26d9c1f5094909c2ce
Michelle Chou <chou.m.rdh@gmail.com>
2020-12-27 08:11:23 -0700

    mongodb

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const { request } = require("express");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000
@@ -16,8 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-// mongodb+srv://dbUser:P@ssword@budgettracker.moaba.mongodb.net/<dbname>?retryWrites=true&w=majority
-// mongodb://heroku_jwcnxtsp:f3gdot37rl25o9vdk54c55iede@ds259528.mlab.com:59528/heroku_jwcnxtsp
+
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
