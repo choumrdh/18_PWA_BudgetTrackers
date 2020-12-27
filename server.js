9d25a6df9ac89af3cd25845987eeaebeef43b363
Michelle Chou <chou.m.rdh@gmail.com>
2020-12-27 08:02:56 -0700

    mongodb atlas

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const { request } = require("express");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000
 
@@ -14,13 +16,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
-
+// mongodb+srv://dbUser:P@ssword@budgettracker.moaba.mongodb.net/<dbname>?retryWrites=true&w=majority
+// mongodb://heroku_jwcnxtsp:f3gdot37rl25o9vdk54c55iede@ds259528.mlab.com:59528/heroku_jwcnxtsp
 let MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
-});
+})
+.then(()=>console.log("MongoDb is connected"))
+.catch((err)=>console.log(err));
 
 // routes
 app.use(require("./routes/api.js"));
